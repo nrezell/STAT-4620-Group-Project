8911e2c8701f51962869ccdfe24b3a56f2ba6ae8
connor-mcneill <mcneill.61@osu.edu>
2022-11-16 18:56:28 -0500

    add changes from mtg

--- Connor EDA for Project.R
+++ Connor EDA for Project.R
@@ -63,3 +63,52 @@ train_upd %>%
 train_upd %>% 
   ggplot() +
   geom_jitter(aes(x=OverallCond, y=OverallQual))
+
+train_upd %>%
+  group_by(OverallCond) %>% 
+  summarise(mean_cond = mean(OverallQual)) %>% 
+  ggplot() +
+  geom_point(aes(x=OverallCond, y=mean_cond))
+
+train_upd %>% 
+  ggplot() +
+  geom_histogram(aes(x=SalePrice)) +
+  scale_x_log10()
+
+num_cols <- unlist(lapply(train, is.numeric))
+data_num <- train[,num_cols]
+
+data_fac <- train[,num_cols==F]
+
+DF <- as.data.frame(unclass(data_fac),stringsAsFactors=TRUE)
+train_df <- cbind(data_num, DF)
+
+traindf_upd <- train_df %>% 
+  mutate(PoolQC = replace_na("None"),
+         MiscFeature = replace_na("None"),
+         Alley = replace_na("None"),
+         Fence = replace_na("None"),
+         FireplaceQu = replace_na("None"),
+         LotFrontage = replace_na(0),
+         GarageType = replace_na("None"),
+         GarageFinish = replace_na("None"),
+         GarageQual = replace_na("None"),
+         GarageCond = replace_na("None"),
+         BsmtExposure = replace_na("None"),
+         BsmtFinType2 = replace_na("None"),
+         BsmtQual = replace_na("None"),
+         BsmtCond = replace_na("None"),
+         BsmtFinType1 = replace_na("None"),
+         GarageYrBlt = case_when(
+           is.na(GarageYrBlt) ~ 0,
+           GarageYrBlt <=1961 ~ 1,
+           GarageYrBlt <=1980 ~ 2,
+           GarageYrBlt <=2002 ~ 3,
+           GarageYrBlt <=2010 ~ 4
+         ),
+         MasVnrType = replace_na("None"),
+         MasVnrArea = replace_na(0)
+  )
+
+library(glmnet)
+
